1166cd13e74762a704d58274db503777cce3aee9
gdesousa <gdesousa@eryxsoluciones.com.ar>
2022-05-18 11:37:58 -0300

    - Add things for SSL

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,8 +8,8 @@ fi
 domains=(programacionpython.ecyt.unsam.edu.ar)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="germandesousa@gmail.com" # Adding a valid address is strongly recommended
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
